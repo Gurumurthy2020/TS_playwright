71f6f133925310a188d60e5c65c70992b3c36d11
Guru <Gurumurthy2485@gmail.com>
2024-07-13 08:15:16 +0530

    ts

---
+++ name.js
@@ -0,0 +1,10 @@
+console.log("hello we are learning typescript");
+
+function isNameexist(){
+    if (number>10) {
+        console.log("its odd");
+    } else {
+        console.log("its even");
+    }
+}
+isNameexist();
---
+++ name.ts
@@ -0,0 +1,3 @@
+console.log("hello we are learning typescript");
+
+// ts will help to find error during compile time itself but js can able to find in runtime
