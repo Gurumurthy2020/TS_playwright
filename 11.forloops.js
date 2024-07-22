5fad9704726603fb2cc02a3251bed7c4e0d5a22f
Guru <Gurumurthy2485@gmail.com>
2024-07-22 21:58:42 +0530

    for

---
+++ 11.forloops.js
@@ -0,0 +1,27 @@
+// for of loops
+var tv = ["LG", "MI", "SAM", " u"];
+for (var _i = 0, tv_1 = tv; _i < tv_1.length; _i++) {
+    var t = tv_1[_i];
+    console.log(t);
+}
+// for in -it will return index
+var Latesttv = ["ab", "sd", "tg", " udf"];
+for (var t in tv) {
+    console.log(t);
+}
+//while loop - no of iteration is not fixed
+//wait for page loading
+//wait for element on page
+//webtable
+//for loop - iteration is fixed
+var i = 1;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+console.log("------do while");
+var d = 1;
+do {
+    console.log(d);
+    d++;
+} while (d <= 10);
---
+++ 11.forloops.ts
@@ -0,0 +1,32 @@
+// for of loops
+
+let tv=["LG","MI","SAM"," u"];
+for (let t of tv) {
+    console.log(t);
+}
+
+// for in -it will return index
+let Latesttv=["ab","sd","tg"," udf"];
+for (let t in tv) {
+    console.log(t);
+}
+
+//while loop - no of iteration is not fixed
+//wait for page loading
+//wait for element on page
+//webtable
+
+//for loop - iteration is fixed
+
+let i =1;
+while (i<=10) {
+    console.log(i);
+    i++;
+}
+
+console.log("------do while");
+let d=1;
+do {
+    console.log(d);
+    d++;
+} while (d<=10);
