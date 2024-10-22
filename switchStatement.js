a2d547d1268daf3c06a350aa7edc7290c7f8f365
Guru <Gurumurthy2485@gmail.com>
2024-10-22 09:42:26 +0530

    switch

---
+++ switchStatement.js
@@ -0,0 +1,12 @@
+var browser = "IE";
+switch (browser) {
+    case "chrome":
+        console.log("we are in chrome");
+        break;
+    case "edge":
+        console.log("we are in edge");
+        break;
+    default:
+        console.log("we are not in chrome or edge");
+        break;
+}
